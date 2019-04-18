88a72ca2b509ce8d5f7e5e141f8f360c1c86ae38
Thienan Nguyen <thienan.nguyen@ousd.us>
2019-04-18 00:36:32 +0000

    Finished search, random, back, and forward buttons. Working on pause/play. Still not sure about music.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var songindex = 0;
+
 $("#search-btn").click(function(){
   user_input = $("#input").val();
   
@@ -5,7 +7,13 @@ $("#search-btn").click(function(){
     url: "https://api.soundcloud.com/tracks?q="+user_input+"&client_id=5aa8e389ba4e24b6106af5159ab3e344",
     method: "GET",
     success: function(response){
-      $("#image").html("<img src='"+response[0].artwork_url+"'class='img-thumbnail'>")
+      songindex = 0;
+      $("#image").html("<img src='"+response[songindex].artwork_url+"'class='img-thumbnail'>")
+      if (response[songindex].artwork_url == null){
+        $("#image").html("<img src='https://www.nasselquistjewellers.com/static/images/temp-inventory-landing.jpg' class='img-thumbnail'>")
+      }
+      
+      $("#title").html("<p class='text-center'>"+response[songindex].title+"</p>")
     }
   });
 });
@@ -15,9 +23,72 @@ $("#random-btn").click(function(){
     url: "https://api.soundcloud.com/tracks?client_id=5aa8e389ba4e24b6106af5159ab3e344",
     method: "GET",
     success: function(response){
-      length = response.length;
-      $("#image").html("<img src='"+response[0].artwork_url+"'class='img-thumbnail'>")
+      var length = response.length;
+      songindex = Math.floor(Math.random() * length);
+      $("#image").html("<img src='"+response[songindex].artwork_url+"'class='img-thumbnail'>")
+      if (response[songindex].artwork_url == null){
+        $("#image").html("<img src='https://www.nasselquistjewellers.com/static/images/temp-inventory-landing.jpg' class='img-thumbnail'>")
+      }
+      
+      $("#title").html("<p class='text-center'>"+response[songindex].title+"</p>")
     }
   });
 });
 
+$("#forward").click(function(){
+  user_input = $("#input").val();
+  
+  $.ajax({
+    url: "https://api.soundcloud.com/tracks?q="+user_input+"&client_id=5aa8e389ba4e24b6106af5159ab3e344",
+    method: "GET",
+    success: function(response){
+      songindex = songindex+1;
+      $("#image").html("<img src='"+response[songindex].artwork_url+"'class='img-thumbnail'>")
+      if (response[songindex].artwork_url == null){
+        $("#image").html("<img src='https://www.nasselquistjewellers.com/static/images/temp-inventory-landing.jpg' class='img-thumbnail'>")
+      }
+      
+      $("#title").html("<p class='text-center'>"+response[songindex].title+"</p>")
+    }
+  });
+});
+
+$("#backward").click(function(){
+  user_input = $("#input").val();
+  
+  $.ajax({
+    url: "https://api.soundcloud.com/tracks?q="+user_input+"&client_id=5aa8e389ba4e24b6106af5159ab3e344",
+    method: "GET",
+    success: function(response){
+      songindex = songindex-1;
+      $("#image").html("<img src='"+response[songindex].artwork_url+"'class='img-thumbnail'>")
+      if (response[songindex].artwork_url == null){
+        $("#image").html("<img src='https://www.nasselquistjewellers.com/static/images/temp-inventory-landing.jpg' class='img-thumbnail'>")
+      }
+      
+      $("#title").html("<p class='text-center'>"+response[songindex].title+"</p>")
+    }
+  });
+});
+
+var myAudio = document.getElementById("music");
+var isPlaying = true;
+
+function togglePlay() {
+  if (isPlaying) {
+    myAudio.pause();
+  } else {
+    myAudio.play();
+  }
+}
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
+
+$("#pause").click(function(){
+  togglePlay();
+});
